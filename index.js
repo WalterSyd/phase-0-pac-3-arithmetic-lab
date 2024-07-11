12ef1fad1f0912fa9bdb00ca53f7dd27182a9eca
Waltersyd <sydney.onyango@student.moringaschool.com>
2024-07-11 17:26:49 +0300

    Index.js

---
+++ index.js
@@ -0,0 +1,70 @@
+//1) 'add()' is a valid function
+function add(){
+    return 
+};
+
+
+//2) 'subtract()' is a valid function
+function subtract(){
+
+};
+
+
+//3) 'multiply()' is a valid function
+function multiply(){
+
+};
+
+
+//4) 'divide()' is a valid function
+function divide(){
+
+};
+
+
+//5) add(a, b) adds two numbers and returns the result
+function add(a, b){
+    return a + b
+}
+
+
+//6) subtract(a, b) subtracts b from a and returns the result
+function subtract(a, b){
+    return a - b
+};
+
+
+//7) multiply(a, b) multiplies two numbers and returns the result
+function multiply(a, b){
+return a * b
+};
+
+// 8) divide(a, b) divides a by b and returns the result
+function divide(a, b){
+return a / b
+};
+
+
+//9) increment(n) increments n and returns the result
+function increment(n){
+return n += 1
+}
+
+
+// 10) decrement(n) decrements n and returns the result
+function decrement(n){
+    return n -= 1
+    }
+
+
+
+// 11) makeInt(n)
+function makeInt(n){
+    return parseInt(n, 10)
+}
+
+
+//12) preserveDecimal(n)
+function preserveDecimal(n){
+    return parseFloat(n)
+}
